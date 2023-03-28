cd8342a620616cc1663c0006dca47768bdca54cf
in12888 <in12888@217ac618-4e2a-4dff-b408-48703624fb97>
2023-03-28 14:00:45 +0000

    récuperer tous les accidents

--- trunk/page_carte/JavaScript/get-accidents.js
+++ trunk/page_carte/JavaScript/get-accidents.js
@@ -1,7 +1,29 @@
 var listDepartement=[]  
-var listAccident=[]  
+var listAccident=[] 
+var listAnnee=[]
+
+
+async function getAnnee(){
+    var api="https://public.opendatasoft.com/api/records/1.0/search/?dataset=accidents-corporels-de-la-circulation-millesime&q=&row=0&facet=datetime"
+
+    try{
+        var res= await fetch(api)
+        var data=await res.json()
+        console.log(data)
+
+        for(var i=0;i<data.facet_groups[0].facets.length;i++){
+            listAnnee.push(data.facet_groups[0].facets[i].name)
+        }
+    }
+    catch{
+        console.log("erreur de fetch")
+    }
+    console.log(listAnnee)
+}
 
 async function getDepartement(){  //recuperer les departements
+
+    
     var api="https://public.opendatasoft.com/api/records/1.0/search/?dataset=accidents-corporels-de-la-circulation-millesime&q=&row=0&facet=dep_name"
     try{
 
@@ -29,32 +51,42 @@ async function getDepartement(){  //recuperer les departements
 
 
 
-async function getAccident(){  //faire des appels par departement mais limite le nombre d'accident par departement a 10000
+async function getAccident(){  //faire des appels par departement et par année pour etre sur d'avoir moin de 10000 accident a la fois
+    loadCarte()
     await getDepartement()
+    await getAnnee()
+
+    
+    
+    
     var api="https://public.opendatasoft.com/api/records/1.0/search/?dataset=accidents-corporels-de-la-circulation-millesime&q=&rows=9999&refine.dep_name="
 
   
-    for(var i=0;i<listDepartement.length;i++){
-        console.log(listDepartement[i])
-        console.log(i)
-        try{
-            
-            
-            var res=await fetch(api+listDepartement[i])
-            var data=await res.json()
-            
-            for(var j=0;j<data.records.length;j++){
-                listAccident.push(data.records[j])
+    for(var i=0;i<listDepartement.length;i++){ //iterer tous les departements
+        for(var j=0;j<listAnnee.length;j++){  //iterer les années
+            console.log(listDepartement[i])
+            console.log(i)
+            try{
+                console.log(listDepartement[i]+" année : "+listAnnee[j])
+                console.log(i)
+                
+                var res=await fetch(api+listDepartement[i]+"&refine.datetime"+listAnnee[j])
+                var data=await res.json()
+                
+                for(var j=0;j<data.records.length;j++){
+                    listAccident.push(data.records[j])
+                }
+                //console.log(listAccident)
+                
+                
             }
-            //console.log(listAccident)
-            
-            
-        }
-        catch{
-            console.log("erreur de fetch")
+            catch{
+                console.log("erreur de fetch")
+            }
         }
     }
-    console.log(listAccident)
+    console.log(listAccident.length)
+    createPin()
 }
 
 
