59f2fb3eb0c12bd02531b22a2ed324893136fe75
in06295 <in06295@217ac618-4e2a-4dff-b408-48703624fb97>
2023-03-29 14:27:28 +0000

    filtres Departements et Date

---
+++ filtres.js
@@ -0,0 +1,41 @@
+var listAccidentFiltre=[]
+
+var listAccidentDate=[]
+var choix_date=(document.getElementById("choix-date")).value;
+
+var listAccidentDepartement=[]
+var choix_departement=(document.getElementById("departement")).value;
+
+function getDataFiltre(){
+    listAccidentDate=[];
+if(choix_date=="date-specifique"){
+    for(var i=0;i<listAccident.length;i++){
+        if((listAccident[i].fields.datetime).substring(0,9)==selectedDate){
+            listAccidentDate.push(listAccident[i]);
+        }
+    }
+}
+else if (choix_date=="intervalle-dates"){ //Comment comparer des dates ?
+    for(var i=0;i<listAccident.length;i++){
+}
+}
+
+listAccidentDepartement=[]
+if(selectedDepartement)
+       for(var i=0;i<listAccident.length;i++){
+        if(selectedDepartement==listAccident[i].fields.dep){//Problème departement en code ici et en nom autre part
+
+        }
+       } 
+}
+
+
+function selectDataFiltre(){
+    if(!choix_departement||choix_departement=="allDepartements"){
+        listAccidentDepartement=listAccident;
+    }
+    if(choix_date==""){
+        listAccidentDate=listAccident;
+    }
+    listAccidentFiltre=listAccidentDate.filter(x=>listAccidentDepartement.includes(x));
+}
