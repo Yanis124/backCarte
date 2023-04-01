c005134d9ce034c063629900b752586554b88930
in06295 <in06295@217ac618-4e2a-4dff-b408-48703624fb97>
2023-04-01 09:59:20 +0000

    filtres département et date

--- branches/carte/filtres.js
+++ branches/carte/filtres.js
@@ -15,13 +15,20 @@ if(choix_date=="date-specifique"){
         }
     }
 }
-else if (choix_date=="intervalle-dates"){ //Comment comparer des dates ? AAAA-MM-JJ 
+else if (choix_date=="intervalle-dates"){ //Comment comparer des dates ? AAAA-MM-JJ
+    const date_debut=new Date(selectedDateStart);
+    const date_fin=new Date(selectedDateEnd);
     for(var i=0;i<listAccident.length;i++){
+        var date_accident=new Date((listAccident[i].fields.datetime).substring(0,9));
+        if(date_debut.getTime()<=date_accident.getTime()&&date_fin.getTime()>=date_accident.getTime()){
+            listAccidentDate.push(listAccident[i]);
+        }
 }
 }
 
 listAccidentDepartement=[]
-if(selectedDepartement)
+if(selectedDepartement){
+
        for(var i=0;i<listAccident.length;i++){
         if(selectedDepartement==listAccident[i].fields.dep_name){
             listAccidentDepartement.push(listAccident[i]);   
@@ -29,7 +36,7 @@ if(selectedDepartement)
        } 
 }
 
-
+}
 function selectDataFiltre(){
     if(!choix_departement||choix_departement=="allDepartements"){
         listAccidentDepartement=listAccident;
@@ -37,5 +44,7 @@ function selectDataFiltre(){
     if(choix_date==""){
         listAccidentDate=listAccident;
     }
-    listAccidentFiltre=listAccidentDate.filter(x=>listAccidentDepartement.includes(x));
+    listAccidentFiltre=listAccidentFiltre.filter(x => listAccidentDate.includes(x));
+    listAccidentFiltre=listAccidentFiltre.filter(x => listAccidentDepartement.includes(x));
+
 }
