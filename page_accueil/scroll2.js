9f842c061fa7da801fe8cdacaa42264a71d8e516
in06295 <in06295@217ac618-4e2a-4dff-b408-48703624fb97>
2023-04-13 07:54:04 +0000

    footer header

---
+++ page_accueil/scroll2.js
@@ -0,0 +1,78 @@
+
+
+const nextBtn = document.querySelector('.right-button');
+const prevBtn = document.querySelector('.left-button');
+const images = document.querySelectorAll('img');
+
+let first=images.length-1
+
+let currentIndex = images.length-1;
+
+function slideIn() {
+
+    if(first<=0){
+
+      images[currentIndex].classList.add('slide-in');
+    }
+
+  
+
+  
+  
+  
+}
+
+function slideOut() {
+  images[currentIndex].classList.add('slide-out');
+  
+  
+}
+
+function slideOutPrev(){
+    console.log(currentIndex)
+    images[currentIndex].classList.add('slide-out-prev')
+    
+    
+}
+
+
+function changeImage() {
+  images[currentIndex].classList.remove('slide-in', 'slide-out','slide-out-prev');
+  
+  
+  
+}
+
+nextBtn.addEventListener('click', () => {
+
+  slideOut();
+  first--;
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  slideIn();
+  setTimeout(changeImage, 500);
+});
+
+prevBtn.addEventListener('click', () => {
+  slideOutPrev();
+
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  slideIn();
+  setTimeout(changeImage, 500);
+  
+});
+
+prevBtn.click()
+ 
+
+setInterval(function(){
+  prevBtn.click()
+},5000)
+
+
+
