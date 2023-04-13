8ae222cf1cad56a3bff261f8c04cc8db27c4bf67
in06298 <in06298@217ac618-4e2a-4dff-b408-48703624fb97>
2023-04-13 15:40:45 +0000

    changement nom téléchargement

--- trunk/page_statistique/Javascript/graph-download.js
+++ trunk/page_statistique/Javascript/graph-download.js
@@ -3,7 +3,7 @@
 function download(){
     const imageLink=document.createElement("a")
     const canvas=document.getElementById("canvas")
-    imageLink.download="canvas.png"
+    imageLink.download= titles+".png"
     imageLink.href=canvas.toDataURL("image/png",1)
 
     imageLink.click()
